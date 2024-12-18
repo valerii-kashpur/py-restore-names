eaaa625bc8edbc8218732eed33418239bec73294
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-18 19:21:46 +0200

    restore names tests

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,24 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names_assigns_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_names_create_and_fill_key_with_value() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Mike"
